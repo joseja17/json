ff528af47fd0a895edf7219194c60c62c5686889
José Jaime Carvajal Calixto <joseja_17@hotmail.com>
2018-04-13 15:57:00 -0600

    adding my responses

--- json.js
+++ json.js
@@ -3,7 +3,7 @@
 ///////////////////Problem 1//////////////////
 
 //Uncomment the example with correct JSON formatting
-// var correctJSON = { "sky": "blue", "earth": "red", "trees": "green", "snow": "white", }
+var correctJSON = { "sky": "blue", "earth": "red", "trees": "green", "snow": "white", }
 // var correctJSON = { sky: "blue", earth: "red", trees: "green", snow: "white" }
 // var correctJSON = { "sky": "blue", "earth": "red", "trees": "green", "snow": "white" }
 // var correctJSON = { sky: "blue", earth: "red", trees: "green", snow: "white", }
@@ -24,7 +24,7 @@
 
 //Which lines contain invalid JSON format? Remove the valid line numbers from the array, 
 //leaving only the line numbers with invalid JSON in the array invalidJSONLineNumbers
-var invalidJSONLineNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
+var invalidJSONLineNumbers = [ 3, 5, 6, 7]
 
 
 
@@ -33,6 +33,8 @@ var invalidJSONLineNumbers = [1, 2, 3, 4, 5, 6, 7, 8]
 //What are some common JSON format errors? Remove the ones that are NOT 
 //JSON format errors, leaving only descriptions of JSON format errors 
 //in the array badJSON.
-var badJSON = ["using curly brackets", "using single quotes", "quotations on everything", "no quotes on keys", "trailing commas", "always use colons", "missing commas"]
+var badJSON = [ "using single quotes",  "no quotes on keys", "trailing commas",  "missing commas"]
+// var badJSON = ["using curly brackets", "using single quotes", "quotations on everything", "no quotes on keys", "trailing commas", "always use colons", "missing commas"]
+
 
 
